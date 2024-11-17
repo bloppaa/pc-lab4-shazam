3be76a0e783dafc85b7c9034fc914b9b5d976fa5
Pablo <pablocortes1101@gmail.com>
2024-11-16 23:00:37 -0300

    feat: add function to create spectrogram from audio file

---
+++ spectrogram.py
@@ -0,0 +1,31 @@
+import numpy as np
+import librosa
+
+
+def generate_spectrogram(
+        audio_path: str | None = None) -> tuple[np.ndarray, int]:
+    """
+    Generate a spectrogram from an audio file.
+
+    Args:
+        audio_path: Path to the audio file.
+
+    Returns:
+        A 2-tuple containing the spectrogram and the sample rate.
+    """
+    y, sr = librosa.load(
+        audio_path if audio_path else librosa.ex("trumpet"), sr=None)
+    D = librosa.stft(y)
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+
+    return S_db, sr
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    S_db, sr = generate_spectrogram()
+
+    plt.figure()
+    librosa.display.specshow(S_db)
+    plt.show()
